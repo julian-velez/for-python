6f759ef6bc58bc4322ca89405c610d6b0d2663e0
julian-velez <60607869+julian-velez@users.noreply.github.com>
2025-06-18 11:31:26 -0500

    for-python
    for-python

---
+++ for/recorrido.py
@@ -0,0 +1,7 @@
+# Usamos un bucle for para recorrer cada letra de la cadena "julian"
+for nombre in "julian":
+    # En cada vuelta del bucle, la variable 'nombre' toma el valor de una letra de "julian"
+    
+    # Imprimimos la letra actual
+    print(nombre)
+
